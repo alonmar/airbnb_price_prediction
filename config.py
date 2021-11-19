0ede8ac8d72a9354ab56ecf24ac65d40396f5a73
Alonso Melgar Lopez <alonmar3@gmail.com>
2021-11-18 22:59:15 -0600

    updated database

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ PWD = os.path.abspath(os.curdir)
 # Local
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/dbase.db'.format(PWD)
 # Heroku
-SQLALCHEMY_DATABASE_URI = 'postgresql://aawgcotyznyepz:94fc718866c22631ed23fb0785affe477969b840e7bc3296c3d23fef850748f0@ec2-3-209-38-221.compute-1.amazonaws.com:5432/d8fr8b6rmksa0k'
+SQLALCHEMY_DATABASE_URI = 'postgresql://esuutcqyxjvwyi:a27ea3b932f83d806a10489199d9b4aec2ff081fa5b032831cfede98b321980d@ec2-54-146-82-179.compute-1.amazonaws.com:5432/db72esghmnr6pj'
 
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
